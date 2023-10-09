baf0c54d6e5eb93705f2178e2df6d91b023dc063
Joao.Miranda <joao.miranda@youxgroup.com.br>
2023-10-09 17:05:09 -0300

    # Implementando o login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,15 +3,64 @@ import { ChevronRight, Visibility, VisibilityOff } from '@mui/icons-material';
 import { Button, IconButton, Input, InputAdornment, TextField } from '@mui/material';
 import image from '@/assets/logo.png';
 import Image from "next/image";
-import React from 'react';
+import InputMask from 'react-input-mask';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+// import {setLogin} from "@/services/authetication/authentication"
 export default function Login() {
 
+    const navigate = useRouter();
+    const API_URL = 'http://localhost:1080/api/usuarios/auth';
+    const API_URL_VERIFY = 'http://localhost:1080/api/usuarios/validatorUser';
+    const history = History; 
+    const TOKEN_COOKIE_KEY : string = 'token';
+    const USER_COOKIE_KEY: string  = 'user';
+    const DADOS_USUARIO : string = 'dados_usuario';
+    const [authenticated, setAuthenticated] = useState(false);
     const [showPassword, setShowPassword] = React.useState(false);
     const [password, setPassword] = React.useState('');
+    const [login, setUser] = React.useState('');
     const handleClickShowPassword = () => setShowPassword((show) => !show);
-    function teste() {
-        console.log(password)
+
+    const getloginUser = () =>  {
+        const user = {
+            login: login,
+            senha: password
+        }
+        console.log(user)
+        setLogin(user)
     }
+
+    const setLogin = async (credentials : any) => {
+        try {
+            const response = await axios.post(API_URL, credentials);
+            const token = response.data.token;
+            const user = jwtDecode(token);
+    
+            Cookies.set(TOKEN_COOKIE_KEY, token, { expires: 1, secure: true });
+            Cookies.set(USER_COOKIE_KEY, JSON.stringify(user), { expires: 1, secure: true });
+            setAuthenticated(true);
+            const getUserCookie: string  = Cookies.get('user') ?? '';
+            const userCookie = JSON.parse(getUserCookie);
+            const usuario = userCookie.sub;
+            Cookies.set(DADOS_USUARIO, usuario, { expires: 1, secure: true });
+            navigate.push('/')
+        // //   showLoading();
+        //   setTimeout(() => {
+        //     // hideLoading();
+        //     // messageSuccess();
+        //     // Replace with your routing logic (React Router, for example)
+        //     // history.push('/Despesas');
+        //   }, 2000);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();}
         return (
@@ -25,11 +74,21 @@ export default function Login() {
                 <form action="" method="post"
                 autoComplete="off">
                     <div>
-                        <Input  required id="standard-required" placeholder="Digite o cpf" className='  text-gray-950 w-[100%] p-[10px] mb-[10px] rounded-sm' />
+                        <InputMask required id="standard-required" placeholder="Digite o cpf" 
+                            onChange={e => setUser(e.target.value)}
+                            mask="###.###.###-##"
+                            className='
+                            text-gray-950 
+                            w-[100%] 
+                            p-[10px] 
+                            mb-[10px] 
+                            rounded-sm'
+                        />
                     </div>
                     <div>
                         <Input id="standard-password-input" 
-                            value={password} 
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             endAdornment={
                                 <InputAdornment position="end">
                                     <IconButton
@@ -41,10 +100,11 @@ export default function Login() {
                                     </IconButton>
                                 </InputAdornment>
                             } 
-                            type={showPassword ? 'text' : 'password'} placeholder="Digite a senha"  autoComplete="current-password" required  className='text-gray-950 w-[100%] p-[10px] mb-[10px] rounded-sm' />
+                            type={showPassword ? 'text' : 'password'} placeholder="Digite a senha"  autoComplete="current-password" required  className='text-gray-950 w-[100%] p-[10px] mb-[10px] rounded-sm' 
+                        />
                     </div>
                     <div>
-                        <Button variant="outlined" onClick={() => { teste() }} endIcon={<ChevronRight />}  className='rounded-md p-[10px] w-[100%] border-1 border-black text-gray-950'>Entrar</Button>
+                        <Button variant="outlined" onClick={() => { getloginUser() }}   endIcon={<ChevronRight />}  className='rounded-md p-[10px] w-[100%] border-1 border-black text-gray-950'>Entrar</Button>
                     </div>
                 </form>
             </section>
---
+++ src/services/authetication/authentication.ts
@@ -0,0 +1,121 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+import Login from '@/components/login/Login';
+
+const navigate = useRouter();
+const API_URL = 'http://localhost:1080/api/usuarios/auth';
+const API_URL_VERIFY = 'http://localhost:1080/api/usuarios/validatorUser';
+const history = History; 
+const TOKEN_COOKIE_KEY : string = 'token';
+const USER_COOKIE_KEY: string  = 'user';
+const DADOS_USUARIO : string = 'dados_usuario';
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check for authentication status when the component mounts
+    restoreAuthentication();
+  }, []);
+
+//   const showLoading = () => {
+//     const loadingInstance = Loading.service({
+//       lock: true,
+//       fullscreen: true,
+//       text: 'Carregando...',
+//       spinner: 'el-icon-loading',
+//       background: 'rgba(0, 0, 0, 1.7)',
+//     });
+//     return loadingInstance;
+//   };
+
+//   const hideLoading = () => {
+//     const loadingInstance = showLoading();
+//     loadingInstance.close();
+//   };
+
+  const messageError = () => {
+    console.error({
+      message: 'Usuário inválido!',
+    });
+  };
+
+//   const messageSuccess = () => {
+//     console.success({
+//       message: 'Seja bem-vindo!',
+//     });
+//   };
+
+  // Function to remove all cookies
+  const removeAllCookies = () => {
+    const cookies = Object.keys(Cookies.get());
+    cookies.forEach(cookie => {
+      Cookies.remove(cookie);
+    });
+  };
+
+  const restoreAuthentication = () => {
+    const token = Cookies.get(TOKEN_COOKIE_KEY);
+    const userCookie = Cookies.get(USER_COOKIE_KEY);
+
+    let user = '';
+    if (userCookie !== undefined) {
+      user = JSON.parse(userCookie as string);
+    }
+
+    if (token && user) {
+      setAuthenticated(true);
+    }
+  };
+
+  const clearAuthentication = () => {
+    removeAllCookies();
+    setAuthenticated(false);
+  };
+
+  export const setLogin = async (credentials : any) => {
+    try {
+        const response = await axios.post(API_URL, credentials);
+        const token = response.data.token;
+        const user = jwtDecode(token);
+
+        Cookies.set(TOKEN_COOKIE_KEY, token, { expires: 1, secure: true });
+        Cookies.set(USER_COOKIE_KEY, JSON.stringify(user), { expires: 1, secure: true });
+        setAuthenticated(true);
+        const getUserCookie: string  = Cookies.get('user') ?? '';
+        const userCookie = JSON.parse(getUserCookie);
+        const usuario = userCookie.sub;
+        Cookies.set(DADOS_USUARIO, usuario, { expires: 1, secure: true });
+        navigate.push('/')
+    // //   showLoading();
+    //   setTimeout(() => {
+    //     // hideLoading();
+    //     // messageSuccess();
+    //     // Replace with your routing logic (React Router, for example)
+    //     // history.push('/Despesas');
+    //   }, 2000);
+    } catch (error) {
+      messageError();
+      console.error(error);
+    }
+  };
+
+  const logout = () => {
+    removeAllCookies();
+    // Replace with your routing logic (React Router, for example)
+    navigate.push('/login');
+    clearAuthentication();
+  };
+
+  const verifyUserExpired = async () => {
+    const response = await axios.get(`${API_URL_VERIFY}/${Cookies.get(TOKEN_COOKIE_KEY)}`);
+    if (response.data === false) {
+      removeAllCookies();
+      clearAuthentication();
+    }
+  };
+
