1f9ee04d789c4bccf1517b5eeb92c360e3eed166
Joao.Miranda <joao.miranda@youxgroup.com.br>
2023-10-09 10:31:25 -0300

    #Implementando a tela de login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,23 +1,31 @@
-import { Button } from '@mantine/core';
-
+// import { Button } from '@mantine/core';
+import { ChevronRight } from '@mui/icons-material';
+import { Button, Input, TextField } from '@mui/material';
+import image from '@/assets/logo.png';
+import Image from "next/image";
 export default function Login() {
     return (
-        <div className='formLogin'>
-            <section className='sectionLogin'>
-                <span className='textLogin'>Bem vindo</span>
+        <div>
+        <div className='flex items-center justify-center pb-3 mt-[20vh]' >
+            <Image src={image} alt={'Imagem'} width={300} height={300}/>
+        </div>
+        <div className= "rounded-md flex items-center  justify-center bg-black">
+            <section className="bg-white p-4 rounded-md w-[300px]">
+                <span className="flex justify-center mb-4 text-[24px] text-gray-950">Bem vindo</span>
                 <form action="" method="post"
                 autoComplete="off">
                     <div>
-                        <input type="text" placeholder="digite o cpf" className='input' />
+                        <TextField  required id="standard-required" label="Digite o cpf" variant="standard" className='  text-gray-950 w-[100%] p-[10px] mb-[10px] rounded-sm' />
                     </div>
                     <div>
-                        <input type="password" placeholder="digite a senha"  className='input' />
+                        <TextField id="standard-password-input" label="Digite a senha" type="password" autoComplete="current-password" variant="standard" required  className='text-gray-950 w-[100%] p-[10px] mb-[10px] rounded-sm' />
                     </div>
                     <div>
-                    <Button variant="filled" className='buttonLogin'><span className='spanBottom'>Entrar</span></Button>
+                        <Button variant="outlined"  endIcon={<ChevronRight />}  className='rounded-md p-[10px] w-[100%] border-1 border-black text-gray-950'>Entrar</Button>
                     </div>
                 </form>
             </section>
+        </div>
         </div>
     )
 }
